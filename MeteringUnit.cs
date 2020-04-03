dce2aa5259906e4aef7e3270f021c5f3eb26dfa3
MLR <lyngrosenquistmorten@gmail.com>
2020-04-03 17:30:40 +0200

    Add project files.

---
+++ MeteringUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class MeteringUnit
+    {
+        public long id;
+        public string location;
+        public string model;
+        public string type;
+    }
+}
---
+++ Submission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class Submission
+    {
+        public MeteringUnit MeteringUnit;
+        public SubmissionPrice SubmissionPrice;
+        public DateTime DateTime;
+        public double RessourceUsage;
+        public string UnitOfMeassure;
+    }
+}
