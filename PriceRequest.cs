41805ea9d423395c123d25cac62a17ad1cc2b921
Jonas Hansen <Jonas Hansen@DESKTOP-68JFB9I>
2020-04-07 16:09:17 +0200

    Added PriceRequest

---
+++ PriceRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Models
+{
+    public class PriceRequest
+    {
+        public DateTime DateTime { get; set; }
+        public double RessourceUsage { get; set; }
+        public string UnitOfMeassure { get; set; }
+    }
+}
