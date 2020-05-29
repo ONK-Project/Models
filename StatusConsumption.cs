2d89be81defb5bad94ece1b01b4a8bb5b0b02470
Morten Lyng Rosenquist <MLR@delegate.dk>
2020-05-29 10:17:54 +0200

    Added StatusConsumption

---
+++ StatusConsumption.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    [Serializable]
+    [BsonIgnoreExtraElements]
+    public class StatusConsumption
+    {
+        public string Location { get; set; }
+        public string UnitOfMeassure { get; set; }
+        public string Type { get; set; }
+        public double RessourceUsage { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public ulong EventSequence { get; set; }
+    }
+}
